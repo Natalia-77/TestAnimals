8e18a74c3b8c26ba87d2b7f47809189176e41b67
vlasnata80@gmail.com <vlasnata80@gmail.com>
2022-08-16 23:39:31 +0300

    Add animals behavoir tests

--- TestAnimals/UnitTest1.cs
+++ TestAnimals/UnitTest1.cs
@@ -1,10 +1,44 @@
+using AnimalsTest;
+
 namespace TestAnimals
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void VoiceLion()
         {
+            var lion = new Lion();
+            var actual = lion.Voice();
+            string expectedVoice = "I am a lion - king of jungle";
+            Assert.Equal(expectedVoice, actual);
+
+        }
+
+        [Fact]
+        public void VoiceJaguar()
+        {
+            var jaguar = new Jaguar();
+            var actual = jaguar.Voice();
+            string expectedVoice = "I am jaguar.Dangerous kitty";
+            Assert.Equal(expectedVoice, actual);
+        }
+
+        [Fact]
+        public void DistanceLion()
+        {
+            var lion = new Lion();           
+            var actual = lion.Distance(1);
+            var expectedValue = 80;
+            Assert.Equal(expectedValue, actual);
+            
+        }
+        [Fact]
+        public void NameLion()
+        {
+            var lion = new Lion();
+            var actualName = "Tor";
+            bool valid = lion.IsValidName(actualName);
+            Assert.True(valid);
 
         }
     }
